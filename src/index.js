e8c20860434c4c5bdf373f80e4f5b716616df000
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-12 20:26:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { AuthProvider } from './component/useContext/AuthContext.jsx'
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AuthProvider>
+    <App/>
+  </AuthProvider>, document.getElementById("root"));
